bd94ce226068ae5645a6c51da8a96bd4acf05781
Tomasz Zurawski <mgrtomaszzurawski@gmail.com>
2021-07-13 01:37:11 +0200

    Prepared repository

---
+++ Backend/src/test/java/pl/clanhouse/unifchat/UnifchatApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.clanhouse.unifchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
