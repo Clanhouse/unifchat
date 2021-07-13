3fb09dd156dfdab8bd0288f531ea628ade8e609c
Lev <konopajew86@gmail.com>
2021-07-14 00:52:14 +0200

    checking API response

--- Frontend/src/app/App.tsx
+++ Frontend/src/app/App.tsx
@@ -1,8 +1,22 @@
-import React, { FC } from 'react';
+import axios from 'axios';
+import React, { FC, useEffect, useState } from 'react';
 import '../styles/app.scss';
-
 const App: FC = () => {
-  return <>Unifchat</>;
+  const url = 'http://localhost:8080';
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const checkAPI = async () => {
+      try {
+        const { data } = await axios.get(`${url}/api//dummy-message`);
+        setData(data);
+      } catch (e) {
+        throw e;
+      }
+    };
+    checkAPI();
+  }, []);
+  return <>{JSON.stringify(data)}</>;
 };
 
 export default App;
