53f86d198d6290f228f68d4326c0e074ccec70c5
nezouse <stankiewicz1998@gmail.com>
2021-07-16 19:02:37 +0200

    Add prettier

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,15 +1,13 @@
-import {styled} from "stitches.config"
+import { styled } from "stitches.config";
 
 export default function Home() {
-  return (
-      <Component>Hello, world!</Component>
-  )
+  return <Component>Hello, world!</Component>;
 }
 
-const Component = styled('div',{
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  width: '100vw',
-  height: '100vh'
-})
+const Component = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100vw",
+  height: "100vh",
+});
