1ebe6091a35af99ac2e23a9173f4fd7c43df2e7f
nezouse <stankiewicz1998@gmail.com>
2021-09-23 13:54:35 +0200

    Update deps

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
-import { getCssString } from "../stitches.config";
+import { getCssText } from "../stitches.config";
 
 export default class Document extends NextDocument {
   render() {
@@ -10,7 +10,7 @@ export default class Document extends NextDocument {
           <link rel="shortcut icon" href="/logo.svg" />
           <style
             id="stitches"
-            dangerouslySetInnerHTML={{ __html: getCssString() }}
+            dangerouslySetInnerHTML={{ __html: getCssText() }}
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;600;700&display=swap"
